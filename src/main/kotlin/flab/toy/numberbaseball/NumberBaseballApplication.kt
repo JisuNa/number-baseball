ea3c08cac8e1ef32ee7b8825b0f649fbfc4db9a5
najisu <kuu2002@naver.com>
2022-12-07 09:43:23 -0800

    Init project

---
+++ src/main/kotlin/flab/toy/numberbaseball/NumberBaseballApplication.kt
@@ -0,0 +1,11 @@
+package flab.toy.numberbaseball
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NumberBaseballApplication
+
+fun main(args: Array<String>) {
+	runApplication<NumberBaseballApplication>(*args)
+}
---
+++ src/test/kotlin/flab/toy/numberbaseball/NumberBaseballApplicationTests.kt
@@ -0,0 +1,13 @@
+package flab.toy.numberbaseball
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NumberBaseballApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
